0b0da2de955cdaf5930ce59f2527e9229ec6cd20
Isigba Ambrose <ambrisexyz@gmail.com>
2022-06-24 10:23:07 +0200

    initial commit

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -10,3 +10,31 @@ document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click'
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
 }));
+
+//storage
+
+const name = document.querySelector('#user_name');
+const email = document.querySelector('#user_email');
+const message = document.querySelector('#user-text');
+
+function storage(name, email, message) {
+  const saver = JSON.stringify({ name, email, message });
+  localStorage.setItem('data', saver);
+}
+
+function storeData() {
+  const data = JSON.parse(localStorage.getItem('data'));
+
+  if (data) {
+    name.value = data.name;
+    email.value = data.email;
+    message.value = data.message;
+  }
+
+  name.addEventListener('input', () => storage(name.value, email.value, message.value));
+  email.addEventListener('input', () => storage(name.value, email.value, message.value));
+  message.addEventListener('input', () => storage(name.value, email.value, message.value));
+}
+
+storeData();
+
