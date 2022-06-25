3da39c314d0312b5a0029258448caa97f5a17cf9
Isigba Ambrose <ambrisexyz@gmail.com>
2022-06-25 02:52:21 +0200

    create modal pop up window with js

--- js/modal.js
+++ js/modal.js
@@ -1,20 +1,10 @@
 // modal pop up
-const buttons = document.querySelectorAll('.img-btn');
-const closeModal = document.querySelector('.close');
-buttons.forEach((button) => {
-  button.addEventListener('click', () => {
-    document.getElementById('my-modal').style.display = 'block';
-  });
-});
-closeModal.addEventListener('click', () => {
-  document.getElementById('my-modal').style.display = 'none';
-});
 
 const projectArr = [
   {
-    image: './images/desktop7.png',
+    image: './images/desktop2.png',
     HeadLine: 'Project name goes here',
-    language: ['HTML/CSS', 'Ruby on Rails', 'Javascript', '+1'],
+    language: ['HTML/CSS', 'Ruby on Rails', 'Javascript'],
     firstParagraph: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet quam provident explicabo velit. Laboriosam ipsa veritatis ipsam asperiores nulla distinctio doloribus atque nobis sed recusandae. Necessitatibus ipsam veniam dolorem sapiente!',
     secondParagraph: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet quam provident explicabo velit. Laboriosam ipsa veritatis ipsam asperiores nulla distinctio doloribus atque nobis sed recusandae. Necessitatibus ipsam veniam dolorem sapiente!',
     liveVersion: '#',
@@ -30,7 +20,7 @@ const projectArr = [
     sourceLink: '#',
   },
   {
-    image: './images/desktop3.png',
+    image: './images/desktop2.png',
     HeadLine: 'Project name goes here',
     language: ['HTML/CSS', 'Ruby on Rails', 'Javascript', '+1'],
     firstParagraph: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis doloribus placeat aliquam modi eveniet officia recusandae pariatur, porro eius quod.',
@@ -39,7 +29,7 @@ const projectArr = [
     sourceLink: '#',
   },
   {
-    image: './images/desktop4.png',
+    image: './images/desktop2.png',
     HeadLine: 'Project name goes here',
     language: ['HTML/CSS', 'Ruby on Rails', 'Javascript', '+1'],
     firstParagraph: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis doloribus placeat aliquam modi eveniet officia recusandae pariatur, porro eius quod.',
@@ -48,7 +38,7 @@ const projectArr = [
     sourceLink: '#',
   },
   {
-    image: './images/desktop5.png',
+    image: './images/desktop2.png',
     HeadLine: 'Project name goes here',
     language: ['HTML/CSS', 'Ruby on Rails', 'Javascript', '+1'],
     firstParagraph: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis doloribus placeat aliquam modi eveniet officia recusandae pariatur, porro eius quod.',
@@ -57,20 +47,20 @@ const projectArr = [
     sourceLink: '#',
   },
   {
-    image: './images/desktop6.png',
+    image: './images/desktop2.png',
     HeadLine: 'Project name goes here',
     language: ['HTML/CSS', 'Ruby on Rails', 'Javascript', '+1'],
     firstParagraph: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis doloribus placeat aliquam modi eveniet officia recusandae pariatur, porro eius quod.',
     secondParagraph: 'Minima fuga ex natus unde nostrum excepturi similique deserunt, ipsum quisquam! Voluptates aperiam, autem ex delectus eius aspernatur alias veritatis voluptatem!',
-    liveVersion: '#',
-    sourceLink: '#',
+    liveVersion: 'see live',
+    sourceLink: 'see source',
   },
 ];
 
+
 // project section
 const projectSection = document.querySelector('#project-sections')
-projectSection.innerHTML = `<div id="project-container" class="workpage">
-<h2 id="proj1">Projects</h2>
+projectSection.innerHTML += `<div id="project-container" class="workpage">
 <div class="full-section-div">
   <div id="img-div1" class="imj-project-section">
     <img id="desktop-img1a" src="./images/image_dots.png" alt="" />
@@ -225,15 +215,157 @@ projectSection.innerHTML = `<div id="project-container" class="workpage">
     </div>
     <img
       id="img-geo"
-      src="./images/image-geometry_5.png"
+      src="./images/image-geometry5c.png"
       alt="my last img"/>
   </div>
 </div>
-</div>`
+</div>`;
+
+/*
+<div class="modal-headline">
+      <span class="close">&times;</span>
+      <h2>project  name goes here</h2></div>
+    <div class="modal-list">
+      <ul>
+        <li><a href="#">HTML/CSS</a></li>
+        <li><a href="#">Ruby on Rails</a></li>
+        <li><a href="#">Javascript</a></li>
+      </ul>
+    </div>
+    <div class="modal-img">
+      <div class="" >
+        <img id="desktop1" src="./images/desktop2.png" alt="modal-image"></div>
+      <div class="small-img">
+        <img id="desk1" src="./images/desktop2.png" alt="modal-image">
+        <img id="desk2" src="./images/desktop2.png" alt="modal-image">
+        <img id="desk3" src="./images/desktop2.png" alt="modal-image">
+        <img id="desk4" src="./images/desktop2.png" alt="modal-image">
+        <img id="desk5" src="./images/desktop2.png" alt="modal-image">
+      </div>
+     <div>
+      </div>
+    </div>
+    <div class="modal-para">
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet quam provident explicabo velit. Laboriosam ipsa veritatis ipsam asperiores nulla distinctio doloribus atque nobis sed recusandae. Necessitatibus ipsam veniam dolorem sapiente!</p>
+      <p> Minima odio nisi animi at praesentium quos odit possimus reprehenderit repellat, ipsam saepe est assumenda fugiat qui deleniti sed similique nam tenetur harum quisquam ducimus. Similique eum ab deserunt voluptatibus?</p>
+    </div>
+    <div class="modal-btn">
+      <button type="button" id="modal-btn1">see live <img id="cross-img4" src="./images/Union8.png" alt=""></button>
+      <button type="button" id="modal-btn2">see source<img id ="cross-img3" src="./images/Vector1.png" alt=""></button>
+    </div>
+    <div class="modal-link">
+      <a href=""> &#8592;  <span>previous project</span></a>
+     <a href="">next project <span>&#8594;</span> </a>
+   </div>*/
+
+   const modal = document.querySelector('.modal')
+   modal.classList.add('modal');
+   modal.id = 'my-modal';
+   modal.appendChild(modalContent);
+   const headTag = document.createElement('div');
+   headTag.classlist.add('modal-headline');
+   headTag.appendChild(exitBtn);
+   const exitBtn = document.createElement('span');
+    exitBtn.classList.add('.close');
+    exitBtn.innerText= '&#times';
+    const h = document.createElement('h2');
+    modalContent.appendChild(h);
+    modalContent.appendChild(h);
+    modalContent.appendChild(headTag);
+    headTag.appendChild(h);
+   h.textContent = projectArr[0].HeadLine;
+
+   const divList =document.createElement('div');
+   divList.classList.add('modal-list');
+   modalContent.appendChild(divList);
+   const unOrderList = document.createElement('ul');
+   divList.appendChild(unOrderList);
+   const listItems = document.createElement('li');
+   unOrderList.appendChild(listItems);
+   const link = document.createElement('a');
+   link.innerText=`${projectArr[0].language}`;
+   listItems.appendChild(link);
+   const modalImages= document.createElement('div');
+   modalImages.classList.add('.modal-img');
+   modalContent.appendChild(modalImages);
+   const bgDesk = document.createElement('div');
+   const image = document.createElement('img');
+   modalImages.appendChild(bgDesk);
+   bgDesk.id = 'desktop1';
+   bgDesk.appendChild(image);
+   image.setAttribute('src', `${projectArr[1].image}`);
+   const smDiv = document.createElement('div');
+   smDiv.classList.add('small-img');
+   modalImages.appendChild(smDiv);
+   const desk1 = document.createElement('img')
+   desk1.id = 'desk1';
+   desk1.setAttribute('src', `${projectArr[2].image}`);
+   smDiv.appendChild(desk1);
+  const desk2 = document.createElement('img')
+   desk2.id = 'desk2';
+   desk1.setAttribute('src', `${projectArr[3].image}`);
+   smDiv.appendChild(desk2);
+  const desk3 = document.createElement('img')
+   desk3.id = 'desk3';
+   desk3.setAttribute('src', `${projectArr[4].image}`);
+   smDiv.appendChild(desk3);
+  const desk4 = document.createElement('img')
+  desk4.id = 'desk4';
+  desk4.setAttribute('src', `${projectArr[5].image}`);
+  smDiv.appendChild(desk4);
+ const desk5 = document.createElement('img')
+   desk5.id = 'desk5';
+   desk1.setAttribute('src', `${projectArr[2].image}`);
+   smDiv.appendChild(desk5);
+
+   const modalDescription = document.createElement('div');
+   modalDescription.classList.add('.modal-para');
+   modalContent.appendChild(modalDescription);
+   const para1 = document.createElement('p');
+   para1.classList.add('.modal-para');
+   para1.innerText= `${projectArr[2].firstParagraph}`;
+   modalDescription.appendChild(para1);
+   const para12 = document.createElement('p');
+   para1.classList.add('.modal-para');
+   para1.innerText= `${projectArr[3].secondParagraph}`;
+   modalDescription.appendChild(para2);
+   const modalButtons = document.createElement('div');
+   modalButtons.classList.add('.modal-btn');
+   modalContent.appendChild(modalButtons);
+ const modalBtn1 = document.createElement('button');
+ modalBtn1.id = 'modal-btn1';
+ modalButtons.prepend(modalBtn1);
+ modalBtn1.innerText = `${projectArr[5].liveVersion}`;
+ const modalBtn2 = document.createElement('button');
+ modalBtn2.id = 'modal-btn2';
+ modalButtons.append(modalBtn2);
+ modalBtn2.innerText = `${projectArr[5].sourceLink}`;
+ const modalLink = document.createElement('div');
+ modalLink.classList.add('modal-link');
+ modalContent.appendChild(modalLink);
+ const modalLinkA = document.createElement('a');
+ modalLinkA.innerHTML = '&#8592;  <span>previous project</span>';
+ modalLink.appendChild(modalLinkA);
+ const modalLinkB = document.createElement('a');
+ modalLinkB.innerHTML = 'next project <span>&#8594;</span>';
+ modalLink.appendChild(modalLinkB);
+
+  const modalContent = document.querySelector('.modal-content');
+   projectSection.appendChild(projectContent);
 
 
 
 
+   const buttons = document.querySelectorAll('.img-btn');
+   const closeModal = document.querySelector('.close');
+   buttons.forEach((button) => {
+     button.addEventListener('click', () => {
+       document.getElementById('my-modal').style.display = 'block';
+     });
+   });
+   closeModal.addEventListener('click', () => {
+     document.getElementById('my-modal').style.display = 'none';
+   });
 
 
 
